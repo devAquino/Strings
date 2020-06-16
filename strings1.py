db3ee8c41e5c78cbb8f5cc4baa8b011a9fb642bf
Reginaldo dos Santos Aquino <aquinoreginaldo@gmail.com>
2020-06-16 10:10:19 -0300

    adicionando comentario para melhor entendimento

--- strings1.py
+++ strings1.py
@@ -1,6 +1,9 @@
+# O comando split() criará uma list de strings separadas com índice iniciado a partir de 0
 mes = """janeiro fevereiro marco abril maio junho julho agosto setembro outubro novembro dezembro""".split()
 
+# Permitirá a entrada do dia, mês e ano e tambem da /
 d,m,a = input("dd/mm/aaaa: ").split("/")
 
+#Converterá a entrada do mês em inteiro para o mês por extenso tirado da lista mes
 print(f'{d} de {mes[int(m)-1]} {a}')
 
